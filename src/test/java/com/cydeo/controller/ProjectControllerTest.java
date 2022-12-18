085135e96cf2d7078efa957f2b06d0b534209773
Dilbar <dibux9@gmail.com>
2022-12-18 15:31:12 -0500

    testing

--- src/test/java/com/cydeo/controller/ProjectControllerTest.java
+++ src/test/java/com/cydeo/controller/ProjectControllerTest.java
@@ -5,15 +5,23 @@ import com.cydeo.dto.RoleDTO;
 import com.cydeo.dto.UserDTO;
 import com.cydeo.enums.Gender;
 import com.cydeo.enums.Status;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -23,11 +31,14 @@ class ProjectControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    static String token;
     static UserDTO manager;
     static ProjectDTO project;
 
     @BeforeAll
     static void setUp() {
+
+        token = "Bearer " + "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJlR0JBd1phNW1KNC1sSEtpenVod3NBd1h0R0pSeVNyRFVSUmd5YnAtb3dVIn0.eyJleHAiOjE2NzE0MDk2MzksImlhdCI6MTY3MTM5MTYzOSwianRpIjoiOWFjMzcxNDgtMDUxNS00MjM2LTgwYzAtZDIxZTA1OTFhYzgwIiwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo4MDgwL2F1dGgvcmVhbG1zL2N5ZGVvLWRldiIsImF1ZCI6ImFjY291bnQiLCJzdWIiOiJhYTcwNTQ5NC1kYzgxLTQwODItYTFlOC01YmIwY2QxNjM3ZDciLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJ0aWNrZXRpbmctYXBwIiwic2Vzc2lvbl9zdGF0ZSI6IjU0ZDI2Mzk1LTE4OTYtNDQ3ZS05NTg4LTVjZTEzYWFjMmQzMCIsImFjciI6IjEiLCJhbGxvd2VkLW9yaWdpbnMiOlsiaHR0cDovL2xvY2FsaG9zdDo4MDgxIl0sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJvZmZsaW5lX2FjY2VzcyIsInVtYV9hdXRob3JpemF0aW9uIiwiZGVmYXVsdC1yb2xlcy1jeWRlby1kZXYiXX0sInJlc291cmNlX2FjY2VzcyI6eyJ0aWNrZXRpbmctYXBwIjp7InJvbGVzIjpbIk1hbmFnZXIiXX0sImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoib3BlbmlkIGVtYWlsIHByb2ZpbGUiLCJzaWQiOiI1NGQyNjM5NS0xODk2LTQ0N2UtOTU4OC01Y2UxM2FhYzJkMzAiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicHJlZmVycmVkX3VzZXJuYW1lIjoib3p6eSJ9.QUkiZsGzJ9boq5_gXidQaW9HqbIBPzEKHgkmeVxsu1hAN-MRcA5vGcMV0fl4lnHUg2uC8WTKeo2U66FghD48JwhYPLO-kSQmE7ImPsFd9ilMe5JHQEP1LibU-YcgqNUbKjvNH_JR467M5RPSpoWnJFNweaSz3uvsOiGmQpLqhmYt7o6V89mnjg_oDYw5hPL1o-ikj4uABsyugX0j-Qs18fOW1pn6gubZknay86DSYQAs9dvL_wl5D0supWuidSxP87armnvaB4VupbEeDUNN-eIogEob84Eub_nMygnrF5zti_hMV6Ey9ANdPJW1DjRCqUsMFbWu_LLeZMir7qlm6Q";
 
         manager = new UserDTO(2L,
                 "",
@@ -51,4 +62,69 @@ class ProjectControllerTest {
         );
 
     }
+
+    @Test
+    void givenNoToken_getProjects() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/api/v1/project"))
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    void givenToken_getProjects() throws Exception {
+
+        mvc.perform(MockMvcRequestBuilders.get("/api/v1/project")
+                .header("Authorization", token)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data[0].projectCode").exists())
+                .andExpect(jsonPath("$.data[0].assignedManager.userName").exists())
+                .andExpect(jsonPath("$.data[0].assignedManager.userName").isNotEmpty())
+                .andExpect(jsonPath("$.data[0].assignedManager.userName").isString())
+                .andExpect(jsonPath("$.data[0].assignedManager.userName").value("ozzy"));
+
+    }
+
+    @Test
+    void givenToken_createProject() throws Exception {
+
+        mvc.perform(MockMvcRequestBuilders.post("/api/v1/project")
+                .header("Authorization", token)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(toJsonString(project)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.message").value("Project is successfully created"));
+    }
+
+    @Test
+    void givenToken_updateProject() throws Exception {
+
+        project.setProjectName("API Project-2");
+
+        mvc.perform(MockMvcRequestBuilders.put("/api/v1/project")
+                .header("Authorization", token)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(toJsonString(project)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("Project is successfully updated"));
+
+    }
+
+    @Test
+    void givenToken_deleteProject() throws Exception {
+
+        mvc.perform(MockMvcRequestBuilders.delete("/api/v1/project/" + project.getProjectCode())
+                .header("Authorization", token)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("Project is successfully deleted"));
+    }
+
+    private String toJsonString(final Object obj) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper.writeValueAsString(obj);
+    }
+
 }
